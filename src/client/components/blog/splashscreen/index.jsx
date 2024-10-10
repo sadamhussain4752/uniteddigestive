7e1974ec75ee8eab9d36f80b3e0241edc847600b
thilak-gowda <119557881+thilak-gowda@users.noreply.github.com>
2024-10-10 18:53:23 +0530

    last
    last

--- src/client/components/blog/splashscreen/index.jsx
+++ src/client/components/blog/splashscreen/index.jsx
@@ -1,16 +1,40 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 
 const SplashScreen = () => {
+  useEffect(() => {
+    const videoElement = document.querySelector("video");
+    const audioElement = document.getElementById("audio");
 
+    // Try playing the video and audio after the component mounts
+    videoElement.play().catch(error => {
+      console.log("Video autoplay was prevented:", error);
+    });
+
+    audioElement.play().catch(error => {
+      console.log("Audio autoplay was prevented:", error);
+    });
+  }, []);
 
   return (
     <div className="splash-screen">
       <div className="logo-container">
-        <img
-          src="/assets/img/banner/logogif.gif" // Replace with your image path
-          alt="Logo"
-          className="logo-image"
-        />
+        <video
+          width="320"
+          height="240"
+          autoPlay
+          muted
+          onError={(e) => console.log("Video failed to load", e)}
+        >
+          <source src="/assets/img/bg/splshscreen.mp4" type="video/mp4" />
+          <source src="/assets/img/bg/splshscreen.mp4" type="video/ogg" />
+          {/* Your browser does not support the video tag. */}
+        </video>
+
+        <audio id="audio">
+          <source src="/assets/audio/onload-sound.mp3" type="audio/ogg" />
+          <source src="/assets/audio/onload-sound.mp3" type="audio/mpeg" />
+          {/* Your browser does not support the audio element. */}
+        </audio>
       </div>
     </div>
   );
